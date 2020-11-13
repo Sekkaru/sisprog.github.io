f170a5af05c25ec14c3d8756a4b4616d27104174
Sekkaru <54922699+Sekkaru@users.noreply.github.com>
2020-11-13 15:54:45 +0700

    Update Lab4.c

--- Lab4.c
+++ Lab4.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int Fib(int i)
+{
+int value = 0;
+if(i < 1) return 0;
+if(i == 1) return 1;
+return Fib(i-1) + Fib(i - 2);
+}
+int main()
+{
+int i = 0;
+while(i < 8)
+{
+printf("%s\n","Идет вычисление следующего элемента, ожидайте...");
+printf("Полученный элемент = ");
+printf("%d\n", Fib(i));
+i++;
+}
+return 0;
+}
