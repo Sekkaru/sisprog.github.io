62ca6afcfda5c4b58b95b49ca7c875278e2aaa8c
Sekkaru <54922699+Sekkaru@users.noreply.github.com>
2020-11-13 16:00:14 +0700

    Update Lab4.c

--- Lab4.c
+++ Lab4.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 int Fib(int i)
 {
 int value = 0;
@@ -9,12 +7,17 @@ return Fib(i-1) + Fib(i - 2);
 }
 int main()
 {
+    FILE *fin; 
+    fin = fopen("text.txt", "r");
 int i = 0;
 while(i < 8)
 {
-printf("%s\n","Идет вычисление следующего элемента, ожидайте...");
+/*printf("%s\n","Идет вычисление следующего элемента, ожидайте...");
 printf("Полученный элемент = ");
-printf("%d\n", Fib(i));
+printf("%d\n", Fib(i));*/
+
+printf("%d",Fib(i));
+printf(" ");
 i++;
 }
 return 0;
