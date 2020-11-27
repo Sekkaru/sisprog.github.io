460a07bde5db51ed09eecf0ceaa35f3eb0289666
Sekkaru <54922699+Sekkaru@users.noreply.github.com>
2020-11-27 19:57:34 +0700

    Update Lab4.c

--- Lab4.c
+++ Lab4.c
@@ -1,39 +1,53 @@
+#include <fcntl.h>
 #include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
 
 
-int Fib(int i)
-{
-int value = 0;
-if(i < 1) return 0;
-if(i == 1) return 1;
-return Fib(i - 1) + Fib(i - 2);
+FILE *f;
+sem_t *semaphore1;
+sem_t *semaphore2;
+
+void* th1(void* args) {
+    int i, sum1 = 0;
+    for (i = 0; i < 20; i++) {
+      sem_wait(semaphore2);
+      f = fopen("txt.txt", "a");
+      sum1 += 2;
+      fprintf(f, "Шаг %d суммы = %d \n", i, sum1);
+      fclose(f);
+      sem_post(semaphore1);
+    }
 }
-int main()
-{
-    FILE *fin; 
-    fin = fopen("C:\\Users\\user\\Desktop\\txt.txt", "w");
-int i = 0;
-while(i < 7)
-{
-/*
-printf("%s\n","Идет вычисление следующего элемента, ожидайте...");
-printf("Полученный элемент = ");
-printf("%d\n", Fib(i));
 
-fprintf(fin,"%s\n","Идет вычисление следующего элемента, ожидайте...");
-fprintf(fin,"%s","Полученный элемент = ");
-fprintf(fin,"%d\n", Fib(i));*/
+void* th2(void* args) {
+  int i, sum2 = 0;
+  for (i = 0; i < 20; i++) {
+    sem_wait(semaphore1);
+    f = fopen("txt.txt", "a");
+    sum2 += 10;
+    fprintf(f, "Шаг %d разности = %d \n", i, sum2);
+    fclose(f);
+    sem_post(semaphore2);
+  }
+}
+
+int main() {
     
-fprintf(fin,"%s","Шаг ");
-fprintf(fin,"%d", i);
-fprintf(fin, ": ");
-fprintf(fin,"%s","Вычисление n * (n- 1) = ");
-fprintf(fin,"%d\n",Fib(i));
+    f = fopen("txt.txt", "w");
+    pthread_t thread1;
+    pthread_t thread2;
 
-printf("%d",Fib(i));
-printf(" ");
-i++;
-}
-return 0;
-}
+    semaphore1 = sem_open("/first_sem", O_CREAT, S_IWUSR, 0);
+    semaphore2 = sem_open("/second_sem", O_CREAT, S_IWUSR, 1);
 
+    pthread_create(&thread1, NULL, th1, NULL);
+    pthread_create(&thread2, NULL, th2, NULL);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+ 
+    sem_unlink("/first_sem");
+    sem_unlink("/second_sem");
+    return 0;
+}
